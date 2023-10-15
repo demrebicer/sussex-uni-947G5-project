b4cf30ac0a0fa9fd58f68831548f3d5dc883b5e6
demrebicer <demrebicer33@gmail.com>
2023-10-16 00:34:43 +0100

    Deployment settings

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import { HashRouter } from "react-router-dom"; // Changed BrowserRouter to HashRouter
+import { BrowserRouter } from "react-router-dom";
 import Router from "./router/router";
 
 import { NextUIProvider } from "@nextui-org/react";
@@ -10,10 +10,10 @@ import "./index.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <NextUIProvider>
           <Router />
       </NextUIProvider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/sussex-uni-947G5-project-group-8",
   plugins: [react()],
 })
