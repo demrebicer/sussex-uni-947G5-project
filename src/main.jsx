4ffc09bda9b0ee01893da212f6705bae3e8d9db5
demrebicer <demrebicer33@gmail.com>
2023-10-16 00:48:08 +0100

    Use hashrouter for deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom"; // Changed BrowserRouter to HashRouter
 import Router from "./router/router";
 
 import { NextUIProvider } from "@nextui-org/react";
@@ -10,10 +10,10 @@ import "./index.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <NextUIProvider>
           <Router />
       </NextUIProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
